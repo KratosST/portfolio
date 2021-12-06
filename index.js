e71f73ddc9ca898f02f5f5f2a7d7dd263f239ace
KratosST <zxiao76@gatech.edu>
2021-12-06 00:03:56 -0500

    update all

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 
-let map, heatmapData, heatmapDataByTime, heatmap, hour;
+let map, leftUp, rightBottom, gridSize, hour, time, currentTime, infoWindow;
+let heatmapData, heatmapDataByTime, heatmapByHour, heatmapDataByRegion, dataByRegion, heatmap;
 
 function initMap() {
-    var dataTime = d3.range(0, 155).map(function(d) {
-        return new Date(2009 + d/12, d%12, 1);
+    var dataTime = d3.range(0, 23).map(function(d) { // 155 months
+        return new Date(2021 + d/12, d%12, 1);
     });
 
-    var time = new Date(2018, 11, 1);
+    var time = new Date(2009, 0, 1);
+    var barTime = new Date(2021, 0, 1);
+    var hour = "";
+    // var currentTime = new Date(2009, 11, 1);
     // console.log(time);
     // console.log(time.getYear());
 
     // slider bottom
     var sliderTime = d3
         .sliderBottom()
-        .min(new Date(2009, 0, 1))
-        .max(new Date(2021, 11, 1))
+        .min(new Date(2021, 0, 1))
+        .max(new Date(2022, 11, 1))
         .step(1000 * 60 * 60 * 24)
         .width(800)
         .ticks(12)
         .tickFormat(d3.timeFormat('%Y-%m-%d'))
         .tickValues(dataTime)
-        .default(new Date(2018, 11, 1))
+        .default(barTime)
         .on('onchange', show);
 
     function show(){
         d3.selectAll(".legend")
             .remove();
+        infoWindow.close();
+        
         time = sliderTime.value();
-        // console.log(time.toISOString().slice(0, 10));
+        time.setFullYear(time.getFullYear()-12);
         if (time.toISOString().slice(0, 10) in heatmapData) {
+            heatmapData[time.toISOString().slice(0, 10)].forEach(function(d){
+                if (Array.isArray(d.location)) {
+                    d.location = new google.maps.LatLng(d.location[0], d.location[1]);
+                    d.weight *= 100;
+                    d.maxIntensity = 10000;
+                }
+            });
             heatmap.setData(heatmapData[time.toISOString().slice(0, 10)]);
-            heatmap.setOptions({radius: 2 * 10 ** (map.getZoom()-15) });
+            heatmap.setOptions({radius: 3 * 10 ** (map.getZoom()-16) });
+            // console.log(time.toISOString().slice(0, 10));
+            // console.log(heatmapData[time.toISOString().slice(0, 10)].length);
         } else {
             heatmap.setData([]);
+            // console.log("no data");
         }
     }
 
@@ -43,8 +59,12 @@ function initMap() {
         .attr('height', 80)
         .append('g')
         .attr('transform', 'translate(40,20)');
-
     gTime.call(sliderTime);
+
+    var tip = d3.tip()
+        .attr("id", "tooltip")
+		.attr("class", "d3-tip")
+		.offset([0, 0])
 
     var hourSelect = ["Select the hour period:"];
     for (let hr = 0; hr < 24; hr++) {
@@ -63,13 +83,15 @@ function initMap() {
         .text(function (d) { return d; }) // text showed in the menu
 
     d3.select("#hourSelect").on("change", function(d){
+        infoWindow.close();
+
         hour = this.value;
         if (hour.length == 13) {
             console.log(heatmapDataByTime[time.toISOString().slice(0, 10)].length);
             var minute1 = parseInt(hour.slice(3,5));
             var minute2 = parseInt(hour.slice(11,13));
             if (minute2 == 0) minute2 = 59;
-            var heatmapByHour = [];
+            heatmapByHour = [];
             for (i=0; i<heatmapDataByTime[time.toISOString().slice(0, 10)].length; i++) {
                 if (heatmapDataByTime[time.toISOString().slice(0, 10)][i].getHours() == parseInt(hour.slice(0,2))) {
                     var minute = heatmapDataByTime[time.toISOString().slice(0, 10)][i].getMinutes();
@@ -86,6 +108,17 @@ function initMap() {
     })
 
     const gatech = { lat: 33.7756, lng: -84.3963 };
+    const leftUp = { lat: 33.885846, lng: -84.550489 };
+    const rightBottom = { lat: 33.6009, lng: -84.284975 };
+    const gridSize = [0.0031696245663468403, 0.0030172045454545005]; // lat and long
+
+    infoWindow = new google.maps.InfoWindow({});
+
+    var heatmapData = {};
+    var heatmapDataByTime = {};
+    var heatmapByHour = [];
+    var heatmapDataByRegion = {};
+    var dataByRegion = {};
 
     // Initialize and add the map
     map = new google.maps.Map(document.getElementById("map"), {
@@ -130,86 +163,97 @@ function initMap() {
         map.data.revertStyle();
     });
 
+    map.addListener("click", (mapsMouseEvent) => {
+        var mouseLatLng = mapsMouseEvent.latLng;
+        
+        var index = Math.floor(Math.abs(leftUp.lat - mouseLatLng.toJSON().lat)/gridSize[0]) * 88 + Math.floor(Math.abs(leftUp.lng - mouseLatLng.toJSON().lng)/gridSize[1]);
+        infoWindow.close();
+        contentString = show_string(time, hour, mouseLatLng, null, index, dataByRegion, heatmapDataByRegion, heatmapByHour);
+        infoWindow = new google.maps.InfoWindow({
+            content: contentString,
+            position: mouseLatLng,
+        });
+        infoWindow.open(map);
+    });
+
     map.data.addListener("click", (mapsMouseEvent) => {
         // map.setZoom(8);
         // map.setCenter(marker.getPosition());
-        // Close the current InfoWindow.
         // console.log(mapsMouseEvent.latLng.toString());
-        console.log(mapsMouseEvent.latLng.toJSON());
-        // infoWindow.close();
-        // infoWindow = new google.maps.InfoWindow({
-        //     position: mapsMouseEvent.latLng,
-        // });
-        // infoWindow.setContent(
-        //     JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-        // );
-        // infoWindow.open(map);
+        var mouseLatLng = mapsMouseEvent.latLng;
+
+        // const resultColor = google.maps.geometry.poly.containsLocation(
+        //     mapsMouseEvent.latLng,
+        //     bermudaTriangle
+        // ) ? "blue" : "red";        
+        var index = Math.floor(Math.abs(leftUp.lat - mouseLatLng.toJSON().lat)/gridSize[0]) * 88 + Math.floor(Math.abs(leftUp.lng - mouseLatLng.toJSON().lng)/gridSize[1]);
+        contentString = show_string(time, hour, mouseLatLng, mapsMouseEvent.feature.h, index, dataByRegion, heatmapDataByRegion, heatmapByHour);
+
+        infoWindow.close();
+        infoWindow = new google.maps.InfoWindow({
+            content: contentString,
+            position: mouseLatLng,
+        });
+        infoWindow.open(map);
+        
     });
 
-    const heatmapData = {};
-    const heatmapDataByTime = {};
-    // const dataByRegion = {};
-
+    //// Read and parse by Papa
     // const fs = require('fs');
     // console.log(document.getElementById("file-input"));
     // const file = fs.createReadStream('refined_data.csv');
-    // var count = 0;
-    Papa.parse("https://raw.githubusercontent.com/KratosST/kratosst.github.io/main/refined_data.csv", {
-        download: true,
-        complete: function(results) {
-            array = results.data; 
-            array.forEach(function(d, i){
-                if (i > 0) {
-                    var date = (new Date(d[0] * 1000)).toISOString().slice(0, 10);
-                    const latLng = new google.maps.LatLng(d[1], d[2]);
+    // Papa.parse("https://raw.githubusercontent.com/KratosST/kratosst.github.io/main/refined_data.csv", {
+    //     download: true,
+    //     complete: function(results) {
+    //         array = results.data; 
+    //         array.forEach(function(d, i){
+    //             if (i > 0) {
+    //                 var date = (new Date(d[0] * 1000)).toISOString().slice(0, 10);
+    //                 const latLng = new google.maps.LatLng(d[1], d[2]);
 
-                    // TODO: draw color by region
-                    if ((date in heatmapData) == false) {
-                        heatmapData[date] = [{location: latLng, weight: 0.1}];
-                        heatmapDataByTime[date] = [new Date(d[0] * 1000)];
-                    } else {
-                        heatmapData[date].push({location: latLng, weight: 0.1});
-                        heatmapDataByTime[date].push(new Date(d[0] * 1000));
-                    }
-                }
-            });
+    //                 // TODO: draw color by region
+    //                 if ((date in heatmapData) == false) {
+    //                     heatmapData[date] = [{location: latLng, weight: 0.1}];
+    //                     heatmapDataByTime[date] = [new Date(d[0] * 1000)];
+    //                 } else {
+    //                     heatmapData[date].push({location: latLng, weight: 0.1});
+    //                     heatmapDataByTime[date].push(new Date(d[0] * 1000));
+    //                 }
+    //             }
+    //         });
 
-            heatmap = new google.maps.visualization.HeatmapLayer({
-                data: heatmapData[time.toISOString().slice(0, 10)],
-                dissipating: false,
-                map: map,
-                radius: 2 * 10 ** (map.getZoom()-15),
-            });
-        }
-    })
+    //         heatmap = new google.maps.visualization.HeatmapLayer({
+    //             data: heatmapData[time.toISOString().slice(0, 10)],
+    //             dissipating: false,
+    //             map: map,
+    //             radius: 2 * 10 ** (map.getZoom()-15),
+    //         });
+    //     }
+    // })
 
-    // Promise.all([
-    //     d3.csv("./refined_data.csv")
-    // ]).then (function(array) {
-    //     results = array[0];
-    //     results.forEach(function(d){
-    //         var date = (new Date(d["timestamp"] * 1000)).toISOString().slice(0, 10);
-    //         const latLng = new google.maps.LatLng(d["lat"], d["long"]);
+    d3.json("./refined.json").then(function(data) {
+        dataByRegion = data["dataByRegion"];
+    });
+    d3.json("./sample.json").then(function(data) {
+        heatmapData = data["heatmapData"];
+        heatmapDataByTime = data["heatmapDataByTime"];
+        heatmapDataByRegion = data["heatmapDataByRegion"];
 
-    //         // TODO: draw color by region
-    //         if ((date in heatmapData) == false) {
-    //             heatmapData[date] = [{location: latLng, weight: 0.1}];
-    //             heatmapDataByTime[date] = [new Date(d["timestamp"] * 1000)];
-    //         } else {
-    //             heatmapData[date].push({location: latLng, weight: 0.1});
-    //             heatmapDataByTime[date].push(new Date(d["timestamp"] * 1000));
-    //         }
+        heatmapData[time.toISOString().slice(0, 10)].forEach(function(d){
+            d.location = new google.maps.LatLng(d.location[0], d.location[1]);
+            d.weight *= 100;
+            d.maxIntensity = 10000;
+        });
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatmapData[time.toISOString().slice(0, 10)],
+            dissipating: false,
+            map: map,
+            radius: 3 * 10 ** (map.getZoom()-16),
+        }) 
+    });
 
-    //     });
-
-    //     heatmap = new google.maps.visualization.HeatmapLayer({
-    //         data: heatmapData[time.toISOString().slice(0, 10)],
-    //         dissipating: false,
-    //         map: map,
-    //         radius: 2 * 10 ** (map.getZoom()-15),
-    //     });
-    // });
-
+    // get_heatmap(time, leftUp, gridSize, heatmapData, heatmapDataByTime, dataByRegion);
+    // get_stkde_map(time, leftUp, gridSize, heatmapData, heatmapDataByTime, heatmapDataByRegion);
 
     // Create a <script> tag and set the USGS URL as the source.
     // const script = document.createElement("script");
@@ -217,4 +261,139 @@ function initMap() {
     //     "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
     // document.getElementsByTagName("head")[0].appendChild(script);
 
+}
+
+function get_heatmap(time, leftUp, gridSize, heatmapData, heatmapDataByTime, dataByRegion) {
+    Promise.all([
+        d3.csv("./refined_data.csv")
+    ]).then (function(array) {
+        results = array[0];
+        results.forEach(function(d){
+            var date = (new Date(d["timestamp"] * 1000)).toISOString().slice(0, 10);
+            // const latLng = new google.maps.LatLng(d["lat"], d["long"]);
+
+            var index = Math.floor(Math.abs(leftUp.lat - d["lat"])/gridSize[0]) * 88 + Math.floor(Math.abs(leftUp.lng - d["long"])/gridSize[1]); 
+
+            // TODO: draw color by region
+            // if ((date in heatmapData) == false) {
+            //     heatmapData[date] = [{location: latLng, weight: 0.1}];
+            //     heatmapDataByTime[date] = [new Date(d["timestamp"] * 1000)];
+            // } else {
+            //     heatmapData[date].push({location: latLng, weight: 0.1});
+            //     heatmapDataByTime[date].push(new Date(d["timestamp"] * 1000));
+            // }
+
+            if (index in dataByRegion) {
+                dataByRegion[index].push(date);
+            } else {
+                dataByRegion[index] = [date];
+            }
+
+        });
+
+        console.log(dataByRegion);
+
+        // heatmap = new google.maps.visualization.HeatmapLayer({
+        //     data: heatmapData[time.toISOString().slice(0, 10)],
+        //     dissipating: false,
+        //     map: map,
+        //     radius: 2 * 10 ** (map.getZoom()-15),
+        // });
+    });
+}
+
+function get_stkde_map(time, leftUp, gridSize, heatmapData, heatmapDataByTime, heatmapDataByRegion) {
+    //// Read and parse by d3
+    Promise.all([
+        d3.csv("./filtered_data(1).csv")
+    ]).then (function(array) {
+        results = array[0];
+        results.forEach(function(d){
+            var ts = d["from_time"]/2+d["to_time"]/2;
+            var date = (new Date(ts * 1000)).toISOString().slice(0, 10);
+            const latLng = new google.maps.LatLng(d["from_lat"]/2+d["to_lat"]/2, d["from_lon"]/2+d["to_lon"]/2);
+
+            var index = Math.floor(Math.abs(leftUp.lat - latLng.toJSON().lat)/gridSize[0]) * 88 + Math.floor(Math.abs(leftUp.lng - latLng.toJSON().lng)/gridSize[1]);
+
+            // console.log(latLng);
+
+            // TODO: draw color by region
+            if ((date in heatmapData) == false) {
+                heatmapData[date] = [{location: latLng, weight: d["possibility"]*1}];
+                heatmapDataByTime[date] = [new Date(ts * 1000)];
+            } else {
+                heatmapData[date].push({location: latLng, weight: d["possibility"]*1});
+                heatmapDataByTime[date].push(new Date(ts * 1000));
+            }
+
+            if (index in heatmapDataByRegion) {
+                if (date in heatmapDataByRegion[index]) heatmapDataByRegion[index][date].push(d["possibility"]);
+                else heatmapDataByRegion[index][date] = [d["possibility"]];
+            } else {
+                heatmapDataByRegion[index] = {};
+                heatmapDataByRegion[index][date] = [d["possibility"]];
+            }
+
+        });
+
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: heatmapData[time.toISOString().slice(0, 10)],
+            dissipating: false,
+            map: map,
+            radius: 2 * 10 ** (map.getZoom()-15),
+        });
+
+        console.log(heatmapDataByRegion);
+
+    });
+}
+
+function show_string(time, hour, mouseLatLng, feature = null, index, dataByRegion, heatmapDataByRegion, heatmapByHour) {
+    var currentTime = new Date(2009, 11, 1);
+    var date = time.toISOString().slice(0, 10);
+    showDate = parseInt(date.slice(0,4))+12 + date.slice(4, date.length);
+    var latLng = mouseLatLng.toJSON();
+    var history = 0, current = 0, location = "outside Atlanta", neighborhood = "N/A", prediction = 0.0, risk = "Safe", hourRange = "whole day";
+    const risk_level = {0.0 : ["safe", "#008450"], 0.5 : ["medium", "#EFB700"], 
+        1.0 : ["cafeful", "#EFB700"], 2.0: ["stay inside", "#B81D13"], 5.0: ["dangerous", "#B81D13"]};
+    
+    if (feature != null) {
+        location = feature.NPU + ", " + feature.STATISTICA;
+        neighborhood = feature.NEIGHBORHO;
+    }
+    if (index in dataByRegion) {
+        history = dataByRegion[index].length;
+        current = dataByRegion[index].filter(x => x==date).length;
+    }
+    if (hour.length == 13) {
+        hourRange = hour;
+        // console.log(heatmapByHour);
+        if (current > 0) current = heatmapByHour.length;
+    }
+    if (index in heatmapDataByRegion && date in heatmapDataByRegion[index]) {
+        prediction = heatmapDataByRegion[index][date].reduce((a, b) => parseFloat(a) + parseFloat(b)) / heatmapDataByRegion[index][date].length;
+        prediction = (prediction * 100).toFixed(2);
+    }
+    for (l in risk_level) {
+        if (prediction >= l) risk = risk_level[l];
+    }
+    var contentString =
+        '<div id="bodyContent">' +
+        '<p>Location: ' + latLng.lat + ', ' + latLng.lng + '</b></p>' +
+        '<p>Region: ' + location + '</p>' +
+        '<p><b>Neighborhood: ' + neighborhood + '</b></p>' +
+        "<p><b>Date: " + showDate + "</b></p>" +
+        "<p><b>Hour Range: " + hourRange + "</b></p>" +
+        "<p><b>Grid Index: </b>" + index + "</p>" + 
+        "<p><b>History Records: </b>" + history + "</p>";
+    if (time < currentTime) {
+        contentString += ("<p><b>Records at " + showDate + ": </b>" + current + "</p>");
+        contentString += ("<p><b>Density Analysis at " + showDate + ": </b>" + prediction + "%</p>");
+    } else {
+        contentString += "<p><b>Predicted Probability at " + showDate + ": </b>" + prediction + "%</p>";
+    }
+    contentString += "<p><b>Risk Level at " + showDate + ": </b>" + "<b><span style=color:" + risk[1] + ">" + risk[0] + "</span></b>" + "</p>";
+    contentString += "</div>";
+
+    return contentString;
 }
